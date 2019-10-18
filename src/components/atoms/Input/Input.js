46ac1614393d1abad9f154692f212c38865ceba5
przonsow <piotr.rzonsowski@cognitran.com>
2019-10-18 13:29:48 +0200

    Add search input

--- src/components/atoms/Input/Input.js
+++ src/components/atoms/Input/Input.js
@@ -1,18 +1,31 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
+
+import magnifierIcon from 'assets/icons/magnifier.svg';
 
 const Input = styled.input`
-	padding: 15px 30px;
+	padding: 1.5rem 3rem;
 	font-size: ${({ theme }) => theme.fontSize.s};
 	font-weight: ${({ theme }) => theme.regular};
 	background-color: ${({ theme }) => theme.grey100};
 	border: none;
-	border-radius: 50px;
+	border-radius: 5rem;
 
 	::placeholder {
 		text-transform: uppercase;
-		letter-spacing: 1px;
+		letter-spacing: 0.1rem;
 		color: ${({ theme }) => theme.grey300};
 	}
+
+	${({ search }) =>
+		search &&
+		css`
+			padding: 1rem 2rem 1rem 4rem;
+			font-size: ${({ theme }) => theme.fontSize.xs};
+			background-image: url(${magnifierIcon});
+			background-size: 1.5rem;
+			background-position: 1.5rem 50%;
+			background-repeat: no-repeat;
+		`}
 `;
 
 export default Input;
