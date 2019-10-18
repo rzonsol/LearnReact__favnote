2a3e7340f5bb741e7e6553b34f7d7aeba7552d95
przonsow <piotr.rzonsowski@cognitran.com>
2019-10-18 14:29:44 +0200

    Add icons as button.

---
+++ src/components/atoms/ButtonIcon/ButtonIcon.js
@@ -0,0 +1,15 @@
+import styled from 'styled-components';
+
+const ButtonIcon = styled.button`
+	width: 6.7rem;
+	height: 6.7rem;
+	border-radius: 2rem;
+	background-image: url(${({ icon }) => icon});
+	background-repeat: no-repeat;
+	background-position: 50% 50%;
+	background-size: 40%;
+	background-color: ${({ active }) => (active ? 'white' : 'transparent')};
+	border: none;
+`;
+
+export default ButtonIcon;
---
+++ src/components/atoms/ButtonIcon/ButtonIcon.stories.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+import styled from 'styled-components';
+
+import bulb from 'assets/icons/bulb.svg';
+import logout from 'assets/icons/logout.svg';
+import pen from 'assets/icons/pen.svg';
+import plus from 'assets/icons/plus.svg';
+import twitter from 'assets/icons/twitter.svg';
+import ButtonIcon from './ButtonIcon';
+
+const YellowBackground = styled.div`
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	background-color: ${({ theme }) => theme.primary};
+	width: 50rem;
+	height: 50rem;
+`;
+
+storiesOf('ButtonIcon', module)
+	.addDecorator(story => <YellowBackground>{story()}</YellowBackground>)
+	.add('Bulb - active', () => <ButtonIcon icon={bulb} active />)
+	.add('Logout', () => <ButtonIcon icon={logout} />)
+	.add('Pen', () => <ButtonIcon icon={pen} />)
+	.add('Plus', () => <ButtonIcon icon={plus} />)
+	.add('Twitter', () => <ButtonIcon icon={twitter} />);
