365c74a33589ae182ff44d748dbfa20ff0373fb4
przonsow <piotr.rzonsowski@cognitran.com>
2019-10-18 12:59:24 +0200

    Add Paragraph atom

--- .storybook/config.js
+++ .storybook/config.js
@@ -3,7 +3,7 @@ import { configure, addDecorator } from '@storybook/react';
 import { ThemeProvider } from 'styled-components';
 import { theme } from '../src/theme/mainTheme';
 
-// addDecorator(story => <ThemeProvider theme={theme}>{story()}</ThemeProvider>);
+addDecorator(story => <ThemeProvider theme={theme}>{story()}</ThemeProvider>);
 
 // automatically import all files ending in *.stories.js
 configure(require.context('../src/components', true, /\.stories\.js$/), module);
--- src/components/atoms/Button/Button.stories.js
+++ src/components/atoms/Button/Button.stories.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
-import { withKnobs, select } from '@storybook/addon-knobs';
+import { withKnobs } from '@storybook/addon-knobs';
 
 import Button from './Button';
 
@@ -9,24 +9,8 @@ export default {
 	title: 'Button',
 };
 
-const colorOption = () => {
-	const label = 'Colors';
-	const options = {
-		Primary: 'hsl(49,100%,58%)',
-		Secondary: 'hsl(196,83%,75%)',
-		Tertiary: 'hsl(106,47%,64%)',
-	};
-	const defaultValue = 'hsl(49,100%,58%)';
-	const groupId = 'GROUP-ID1';
-	return select(label, options, defaultValue, groupId);
-};
-
-const primary = () => <Button color={colorOption}>Save / Exit</Button>;
-const secondary = () => (
-	<Button secondary color={colorOption}>
-		Removed
-	</Button>
-);
+const primary = () => <Button>Save / Exit</Button>;
+const secondary = () => <Button secondary>Removed</Button>;
 
 storiesOf('Button', module)
 	.addDecorator(withKnobs)
