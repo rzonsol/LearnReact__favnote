bc681f55a49a2eee60004965f5c42bc67d015502
przonsow <piotr.rzonsowski@cognitran.com>
2019-10-18 11:37:22 +0200

    Add new structure for atomic disigne.

--- src/views/Root/Root.js
+++ src/views/Root/Root.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import Button from 'components/Button/Button';
+import Button from 'components/atoms/Button/Button';
 import GlobalStyle from 'theme/GlobalStyle';
 
 const Root = () => (
